065faa6b2c8e15952d439e1a15bea227a3ee5e6e
Stephen <sw@itembase.biz>
2016-08-16 11:29:21 +0200

    Function to get unigram model. With simple text cleaning (whitespace normalisation + punctuation removal)

--- language-modelling.R
+++ language-modelling.R
@@ -1,7 +1,10 @@
-get_simple_language_model <- function(x){
+get_unigram_model <- function(x){
 
    lc_corpus <- tolower(paste(readLines(x), collapse=" "))
+   
    tokens <- unlist(strsplit(lc_corpus, " "))
+   tokens <- gsub("[\\t\\s]+", " ", tokens, perl=T)
+   tokens<- gsub("[[:punct:]]", " ", tokens)
    n_tokens <- length(tokens)
    frequencies <- sort(table(tokens), decreasing=T)
    language_model <- data.frame(frequencies / n_tokens)
